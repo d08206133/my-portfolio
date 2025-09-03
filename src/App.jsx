55c83d1afcf792acd57579cd1d132d068aceac16
yo <d08206133@gmail.com>
2025-09-03 15:31:46 +0900

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,199 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='text-gray-700 border-b border-gray-200'>
+        <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+          <a href="#" className='font-medium text-gray-900 mb-4 md:mb-0'>
+            <span className='text-xl ml-3'>NisshoCode</span>
+          </a>
+          <nav className='md:ml-auto text-base'>
+            <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>Home</a>
+            <a href="#about" className='mr-5 hover:text-blue-400 duration-300'>About</a>
+            <a href="#skills" className='mr-5 hover:text-blue-400 duration-300'>Skills</a>
+            <a href="#blog" className='hover:text-blue-400 duration-300'>Blog</a>
+          </nav>
+        </div>
+      </header>
+      <section className='text-gray-700' id='home'>
+        <div className='flex container mx-auto py-20 px-5 flex-col md:flex-row items-center'>
+          <div className='md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left'>
+            <h1 className='text-3xl sm:text-6xl text-gray-900 font-medium mb-4'>Yohei Okamura<br />
+              My Portfolio WebSite</h1>
+            <p className='mb-8 leading-relaxed'>
+              現在、Webエンジニアとして日々学習と実践を行っています。<br />
+              得意な言語は、HTML, CSS, JavaScript, TypeScript, PHP, SQL, ReactなどWeb全般とJava, Pythonです。<br />
+              製作物を一部公開しておりますので、ぜひご覧ください。
+            </p>
+            <button className='text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300'>Contact Me</button>
+          </div>
+          <div className='md:w-1/2 lg:max-w-lg w-5/6'>
+            <img src="./img/myPic.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+      <section className='text-gray-700 border-t border-gray-200'>
+        <div className='container px-5 py-24 mx-auto'>
+          <div className='text-center mb-20'>
+            <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900'>About Me</h1>
+            <p className='pb-10'>
+              20代は金融業界の保険分野にて基幹システムの開発・保守運用に従事。現在はスキルアップのためWebサイトやWebAppの個人開発を行っています。<br />
+              新規のお仕事に関するご相談についても積極的に受け付けておりますので、お声掛けいただけますと幸いです。
+            </p>
+            <p>
+              以下に個人としての製作物を公開しています。主にSNSなどのChatアプリの構築をReactのJSやTSで実装しています。<br />
+              また、Pythonを使ってAIによる株価の予測サイトを構築しました。また数件の事業所のWebサイトの構築など多岐にわたります。
+            </p>
+          </div>
+          {/* カードのdivタグ */}
+          <div className='flex flex-wrap'>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 11C13.1046 11 14 10.1046 14 9C14 7.89543 13.1046 7 12 7C10.8954 7 10 7.89543 10 9C10 10.1046 10.8954 11 12 11Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M11 13C11 14.1046 10.1046 15 9 15C7.89543 15 7 14.1046 7 13C7 11.8954 7.89543 11 9 11C10.1046 11 11 11.8954 11 13Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M15 15C16.1046 15 17 14.1046 17 13C17 11.8954 16.1046 11 15 11C13.8954 11 13 11.8954 13 13C13 14.1046 13.8954 15 15 15Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M3 4C3 2.34315 4.34315 1 6 1H18C19.6569 1 21 2.34315 21 4V20C21 21.6569 19.6569 23 18 23H6C4.34315 23 3 21.6569 3 20V4ZM6 3H18C18.5523 3 19 3.44772 19 4V20C19 20.5523 18.5523 21 18 21H6C5.44772 21 5 20.5523 5 20V4C5 3.44772 5.44772 3 6 3Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>SNS Chat App</h2>
+                </div>
+                <div>
+                  <p>
+                    ReactとJavaScriptで投稿機能付きのChatアプリ。<br />
+                    技術仕様はReact, JavaScript, html, css, firebase, AWSなど。<br />
+                    機能についてはログイン・ログアウト認証機能、メッセージ投稿機能、検索機能、お気に入り登録機能を実装しています。
+                  </p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る → </a>
+                </div>
+              </div>
+            </div>
+
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M13 10H17V16H13V10Z" fill="currentColor" fill-opacity="0.5" />
+                      <path d="M11 4H7V16H11V4Z" fill="currentColor" />
+                      <path d="M18 18H6V20H18V18Z" fill="currentColor" />
+                    </svg>
+
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>AI Finance</h2>
+                </div>
+                <div>
+                  <p>
+                    AIを使った株やビットコインのデータの取得、分析、1か月後の予測アプリです。
+                    AIはscikit learnの回帰で実装しています。<br />
+                    主にPythonでコーディングを行い、フレームワークはStreamlitを使用しています。<br />
+                    株や暗号通貨の売買にぜひお役立ててください。
+                  </p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る → </a>
+                </div>
+              </div>
+            </div>
+
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>Web Site Dev</h2>
+                </div>
+                <div>
+                  <p>
+                    中小企業のサイト構築を数件受注しています、商社や税理士事務所、業界団体など多岐にわたります。<br />
+                    それぞれの案件は、綿密にヒアリングを行い、クライアントが納得するまで修正を繰り返します。<br />
+                    自社サーバーを使用することで安価に抑えています。
+                  </p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る → </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='text-gray-700 border-t border-gray-200'>
+        <div className='container px-5 py-24 mx-auto flex flex-wrap'>
+          {/* left side */}
+          <div className='mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center'>
+            <img src="./img/pc.jpg" alt="" className='rounded' />
+          </div>
+          {/* right side */}
+          <div className='lg:pl-12 lg:py-6 w-full lg:w-1/2'>
+            <h1 className='text-2xl sm:text-3xl font-medium text-gray-900 mb-10 text-center lg:text-left'>My Skills</h1>
+            <div className='w-full'>
+              <h2>HTML</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-green-600 text-xs leading-none py-1 text-center text-white' style={{ width: "85%" }}>85%</div>
+              </div>
+
+              <h2>JavaScript</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-orange-600 text-xs leading-none py-1 text-center text-white' style={{ width: "75%" }}>75%</div>
+              </div>
+
+              <h2>React</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-blue-600 text-xs leading-none py-1 text-center text-white' style={{ width: "60%" }}>60%</div>
+              </div>
+
+              <h2>Python</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-yellow-600 text-xs leading-none py-1 text-center text-white' style={{ width: "65%" }}>65%</div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
